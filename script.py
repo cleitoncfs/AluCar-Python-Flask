fe7ed24123e6dbfa7306b4cf5621d3666b674fdd
Cleiton Santos <cleitoncfs@gmail.com>
2024-10-19 23:07:33 +0100

    Limpa o branch gh-pages

--- script.py
+++
@@ -1,7 +0,0 @@
-from flask_frozen import Freezer
-from main import app  # Certifique-se de que 'main' é o nome do seu módulo principal
-
-freezer = Freezer(app)
-
-if __name__ == '__main__':
-    freezer.freeze()
--- views.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from flask import render_template
-
-# rotas
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
